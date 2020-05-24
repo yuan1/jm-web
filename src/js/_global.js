fb24579d810f7334b79f392f1909845534335d49
liuyonghua <lyh@jm.energy>
2020-05-24 14:47:12 +0800

    update scroll

--- src/js/_global.js
+++ src/js/_global.js
@@ -1,10 +1,12 @@
 // Global
+
+// Instantiating the global src object
+var app = {}
 app.global = {
   init: function () {
     // Load all global functions here
     console.log('load global functions')
   },
 }
-
 // Run the global stuff
 app.global.init()
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,22 @@
 console.log('Load script.js')
 
-// Instantiating the global src object
-var app = {}
+var new_scroll_position = 0
+var last_scroll_position
+var header = document.getElementById('header')
+
+window.addEventListener('scroll', function (e) {
+  last_scroll_position = window.scrollY
+
+  // 向下滚动
+  if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
+    header.classList.remove('slideDown')
+    header.classList.add('slideUp')
+
+    // 向上滚动
+  } else if (new_scroll_position > last_scroll_position) {
+    header.classList.remove('slideUp')
+    header.classList.add('slideDown')
+  }
+
+  new_scroll_position = last_scroll_position
+})
